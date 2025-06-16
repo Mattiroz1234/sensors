2707a502f6d8ef9385a1da5af497733fd34d1b44
matti <m0556764778@gmail.com>
2025-06-16 12:35:38 +0300

    sensors

---
+++ AudioSensor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sensors
+{
+    internal class AudioSensor : Sensor
+    {
+        public override string Type => "Audio";
+
+        public override bool IsBroken => false;
+
+        public override bool Activate()
+        {
+            ActivationCount++;
+            return true;
+        }
+    }
+
+}
