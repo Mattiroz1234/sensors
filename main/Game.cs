31842f366dd9ee6a3c0119a02425bd3306b7a837
matti <m0556764778@gmail.com>
2025-06-18 23:18:10 +0300

    Improvements

--- main/Game.cs
+++ main/Game.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using sensors.main;
+using sensors.servis;
 
 namespace sensors
 {
@@ -11,47 +12,11 @@ namespace sensors
     {
         public static void StartGame()
         {
-            IranianAgent agent = new SeniorCommander();
+            List<string> AgentsExposed = new List<string>();
+            IranianAgent agent = AgentServis.AgentMaker();
             Console.WriteLine("Agent captured. Try to uncover his weaknesses.");
-            Investigation(agent);
+            string a = AgentServis.Investigation(agent);
+            AgentsExposed.Add(a);
         }
-
-
-        public static void Investigation(IranianAgent agent)
-        {
-            int totalNeeded = agent.WeaknessCount;
-            var availableSensors = SensorFactory.GetAvailableSensorTypes();
-            int turn = 0;
-
-            while (!agent.IsExposed())
-            {
-                turn++;
-                Console.WriteLine($"\nTurn: {turn}");
-                Console.WriteLine("Available sensors: " + string.Join(", ", availableSensors));
-                Console.Write("Enter sensor type: ");
-                string input = Console.ReadLine();
-
-                Sensor sensor = SensorFactory.CreateSensor(input);
-                if (sensor == null)
-                {
-                    Console.WriteLine("Invalid sensor type.");
-                    turn--;
-                    continue;
-                }
-
-                agent.AttachSensor(sensor);
-                int match = agent.CheckMatch();
-                Console.WriteLine($"Matching sensors: {match}/{totalNeeded}");
-
-                if (turn % 3 == 0 && match != totalNeeded)
-                {
-                    int remuvedSensors = agent.Counterattack();
-                    Console.WriteLine($"Matching sensors: {match - remuvedSensors}/{totalNeeded}");
-                }
-            }
-
-            Console.WriteLine("Agent exposed successfully!");
-        }
-
     }
 }
---
+++ servis/AgentServis.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using sensors.agents;
+using sensors.main;
+
+namespace sensors.servis
+{
+    internal class AgentServis
+    {
+        public static string Investigation(IranianAgent agent)
+        {
+            int totalNeeded = agent.WeaknessCount;
+            var availableSensors = SensorFactory.GetAvailableSensorTypes();
+            int turn = 0;
+
+            while (!agent.IsExposed())
+            {
+                turn++;
+                Console.WriteLine($"\nTurn: {turn}");
+                Console.WriteLine("Available sensors: " + string.Join(", ", availableSensors));
+                Console.Write("Enter sensor type: ");
+                string input = Console.ReadLine();
+
+                Sensor sensor = SensorFactory.CreateSensor(input);
+                if (sensor == null)
+                {
+                    Console.WriteLine("Invalid sensor type.");
+                    turn--;
+                    continue;
+                }
+
+                agent.AttachSensor(sensor);
+                int match = agent.CheckMatch();
+                Console.WriteLine($"Matching sensors: {match}/{totalNeeded}");
+
+                if (turn % 3 == 0 && match != totalNeeded)
+                {
+                    int remuvedSensors = agent.Counterattack();
+                    Console.WriteLine($"Matching sensors: {match - remuvedSensors}/{totalNeeded}");
+                }
+            }
+
+            Console.WriteLine("Agent exposed successfully!");
+            return agent.Rank;
+        }
+
+        private static IranianAgent CreateAgent(string type)
+        {
+            switch (type)
+            {
+                case "Foot Soldier":
+                    return new FootSoldier();
+
+                case "Senior Commander":
+                    return new SeniorCommander();
+
+                case "Squad leader":
+                    return new SquadLeader();
+
+                case "Organization Leader":
+                    return new OrganizationLeader();
+
+                default:
+                    return null;
+            }
+        }
+
+        private static List<string> GetAgentTypes()
+        {
+            return new List<string> { "Foot Soldier", "Senior Commander", "Squad leader", "Organization Leader" };
+        }
+
+        public static IranianAgent AgentMaker()
+        {
+            List<string> agents = GetAgentTypes();
+
+            var rnd = new Random();
+            string randomType = agents[rnd.Next(agents.Count)];
+            IranianAgent agent = CreateAgent(randomType);
+            return agent;
+        }
+    }
+}
